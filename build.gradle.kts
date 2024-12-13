5b10acfb94825a0250dbc3e340fa465a45fb1a68
kzcY3Rr <115343303+kzcY3Rr@users.noreply.github.com>
2024-12-13 11:24:25 +0300

    改进了 validate_user 函数的实现

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(libs.plugins.google.ksp) apply false
+
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.androidx.room) apply false
+
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+
+    alias(libs.plugins.rikka.refine) apply false
+}
+
+// https://kotlinlang.org/docs/js-project-setup.html#use-pre-installed-node-js
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().download =
+        false
+}
